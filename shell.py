e30b4da217148ac34a1483ae80579e0d0b0cdf5c
doubleplusplus <jiadeng@hotmail.com>
2020-05-03 23:52:17 -0700

    initial frameworrk

--- shell.py
+++ shell.py
@@ -1,9 +1,13 @@
 from cmd import Cmd
-
+import os
 
 class MyShell(Cmd):
     prompt = '<Shell> '
     intro = "Welcome! Type ? to list commands. Type 'exit' to end Shell"
+
+    def do_prompt(self, line):
+        "Change the interactive prompt"
+        self.prompt = '<' + line + '> '
 
     def do_exit(self, inp):
         '''exit the Shell. Shorthand: x q.'''
@@ -16,17 +20,24 @@ class MyShell(Cmd):
     def default(self, inp):
         if inp == 'x' or inp == 'q':
             return self.do_exit(inp)
-
         print("Default: {}".format(inp))
-
-    def do_add(self, inp):
-        print("Adding '{}'".format(inp))
-
-    def help_add(self):
-        print("Add a new entry to the system.")
 
     do_EOF = do_exit  # press command+D, send EOF and exit
     help_EOF = help_exit
+
+    last_output = ''
+
+    def do_shell(self, line):
+        "Run a shell command"
+        print("running shell command:", line)
+        output = os.popen(line).read()
+        print(output)
+        self.last_output = output
+
+    def do_echo(self, line):
+        "Print the input, replacing '$out' with the output of the last shell command"
+        # Obviously not robust
+        print(line.replace('$out', self.last_output))
 
 
 if __name__ == '__main__':
