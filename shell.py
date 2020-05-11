5b7eeff491837c3ad07ff908c39497571ccabedd
doubleplusplus <jiadeng@hotmail.com>
2020-05-10 23:47:33 -0700

    fix default

--- shell.py
+++ shell.py
@@ -1,17 +1,28 @@
 from cmd import Cmd
 import os
 import sys
+import shlex
+import signal
+
 
 class MyShell(Cmd):
-    prompt = '<' + os.getcwd() + '> '
+    prompt = ''
     intro = "Welcome! Type ? to list commands. Type 'exit' or 'x' to stop Shell"
+
+    def __init__(self):
+        super(MyShell, self).__init__()
+        self.prompt = '<' + self.full_to_home_abbr(os.getcwd()) + '> '
 
     def do_prompt(self, line):
         "Change the interactive prompt"
         self.prompt = '<' + line + '> '
 
-    def do_exit(self, inp):
+    def do_exit(self, arg):
         '''exit the Shell. Shorthand: x q.'''
+        args = shlex.split(arg)
+        if len(args) > 1:
+            print('exit: too many arguments', file=sys.stderr)
+            return
         print("Shell terminated!")
         return True
 
@@ -26,7 +37,19 @@ class MyShell(Cmd):
     def default(self, inp):
         if inp == 'x':
             return self.do_exit(inp)
-        print("Default: {}".format(inp))
+        #print("Default: {}".format(inp))
+        command = shlex.split(inp)
+        child_pid = os.fork()
+        if child_pid == 0:
+            try:
+                os.execvp(command[0], command)
+            except:
+                print(f'No command: {command[0]}')
+                # if error, kill child process
+                os.kill(os.getpid(), signal.SIGKILL)
+        else:
+            os.waitpid(child_pid, 0)
+
 
     do_EOF = do_exit  # press command+D, send EOF and exit
     help_EOF = help_exit
@@ -59,14 +82,26 @@ class MyShell(Cmd):
         except:
             print('Path not found: {}'.format(input))
 
+    def home_abbr_to_full(self, abbr_path: str):
+        if abbr_path.startswith('~'):
+            abbr_path = abbr_path.replace('~', os.environ['HOME'], 1)
+        return abbr_path
+
+    def full_to_home_abbr(self, full_path: str):
+        if full_path.startswith(os.environ['HOME']):
+            full_path = full_path.replace(os.environ['HOME'], '~', 1)
+        return full_path
+
     def do_cd(self, path):
         """convert to absolute path and change directory"""
         try:
             os.chdir(os.path.abspath(path))
-            self.do_prompt(os.getcwd())
+            self.do_prompt(f'{self.full_to_home_abbr(os.getcwd())}')
             #print(os.getcwd())  # cwd = current working directory
-        except Exception:
-            print("cd: no such file or directory: {}".format(path))
+        except FileNotFoundError:
+            print('cd: not a file: {}'.format(path))
+        except NotADirectoryError:
+            print('cd: not a directory {}'.format(path))
 
 
 if __name__ == '__main__':
@@ -75,5 +110,5 @@ if __name__ == '__main__':
         MyShell().onecmd(' '.join(sys.argv[1:]))
     else:
         MyShell().cmdloop()
-    #os.system('ls')
+
 
